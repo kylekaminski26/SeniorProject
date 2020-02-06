ab9815a59fa68122fea94ddcf79437468faecf3d
John Miller <millerj51@students.rowan.edu>
2020-02-05 23:37:08 -0500

    Large Procedural Map with some items on the board
    Added Player 'Attack' animation

--- Prototype-JM/Assets/Scripts/PlayerMovement.cs
+++ Prototype-JM/Assets/Scripts/PlayerMovement.cs
@@ -2,29 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact
+}
+
 public class PlayerMovement : MonoBehaviour
 {
-
+    public PlayerState currentState;
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
     private Animator animator;
 
     // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
+        currentState = PlayerState.walk;
         animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
     }
 
-    // Update is called once per frame
-    void Update()
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
     {
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
 
-        UpdateAminationAndMove();
+        if (Input.GetButtonDown("Attack") && currentState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        } 
+        else if(currentState == PlayerState.walk)
+        {
+            UpdateAminationAndMove();
+        }
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
     }
 
     void UpdateAminationAndMove()
@@ -44,6 +72,7 @@ public class PlayerMovement : MonoBehaviour
 
     void MoveCharacter()
     {
+        change.Normalize();
         myRigidbody.MovePosition(
                transform.position + change * speed * Time.deltaTime
             );
