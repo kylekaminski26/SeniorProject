07b85005f8e6d1e3b4c39a0447a283f142a6916d
caceresv <59008501+caceresv@users.noreply.github.com>
2020-02-07 16:30:54 -0500

    Prototype
    Base scene created to test out some fundamental things we might need in the game.
    
    Includes a script for WASD movement, for having the camera follow the player, and to allow the player to shoot a projectile.
    
    Uses sprites from assests from the asset store. The scripts are designed to make it easy to swap out these sprites as they are only used now as a way to test the scripts.

---
+++ Test Project (SP)/Assets/Scripts/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int health;      // Player's health
+    public int stamina;     // Player's stamina
+    public int armor;       // Player's armor
+    public float strength;  // Player's strength
+    public float mobility;  // Player's mobility
+
+    int damageDelay = 0;    // Variable used to delay the damage taken by hazards
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 20; 
+        stamina = 100;
+        armor = 5;
+        strength = 1f;
+        mobility = 1f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*  OnTriggerStay is called once per physics update and runs the code if a collider/rigidbody are in contact
+    
+        Currently used to test damage done by hazards
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "Hazard")
+        {
+            damageDelay--;
+            if(damageDelay <= 0)
+            {
+                health--;
+                Debug.Log("Health:" + health);
+                damageDelay = 80;
+            }
+        }
+    }
+
+    */
+}
---
+++ Test Project (SP)/Assets/Scripts/Projectile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    void OnCollision2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+}
