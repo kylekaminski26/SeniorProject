ea6ef0db8b1f65f1582723ea05b001e473c8000d
Chrisfalotico <faloti24@students.rowan.edu>
2020-04-06 12:21:55 -0400

    Added UI (Menu and pause menu)
    Added UI elements to the main game

---
+++ SeniorProject-master/Assets/WorldGen/Scripts/AddRoom.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AddRoom : MonoBehaviour
+{
+    private RoomTemplates templates;
+
+    private void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("RoomTemplate").GetComponent<RoomTemplates>();
+        templates.rooms.Add(this.gameObject);
+    }
+}
