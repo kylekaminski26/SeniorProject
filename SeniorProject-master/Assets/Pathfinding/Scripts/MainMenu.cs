2037d9d3f5aeedc76139c93d06969c2f865a268a
Chrisfalotico <faloti24@students.rowan.edu>
2020-04-11 18:29:12 -0400

    Fixed minor UI issues
    Fixed minor issues with the UI including scaling issues and missing audio.

---
+++ SeniorProject-master/Assets/Pathfinding/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class MainMenu : MonoBehaviour
+{
+
+    AudioSource aud;
+
+    public void NewGame()
+    {
+        aud = GetComponent<AudioSource>();
+        aud.Play();
+	ChangeScene();
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame() 
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    
+}
