d4c97dd4905bb7e18b671592af6bedbc37263c0a
Matthew <kevillem3@students.rowan.edu>
2020-04-11 16:44:09 -0400

    fff
    Replaced previous slime with my slime data. Wired Tim's pathfinding to my slime, changed Rigid Bodies to all be dynamic and imposed drags on player to circumvent infinite force.

---
+++ SeniorProject-master/Assets/Battlers/Scripts/Battler.cs
@@ -0,0 +1,196 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum BattlerState
+{
+    idle,
+    walk,
+    hitStun,
+    attack,
+    dead
+}
+
+/*This class holds all of the common elements
+ * between the player and the enemy
+ * This includes initial battle variables,
+ * states, and routines for recieving damage
+ *
+ * Any entity that fights will inherit from this parent class
+ * All battler must have : a RigidBody2D, an Animator, a Sprite Collider, and Sprite Renderer
+ *
+ * The current Battler Model considers the following Variables {health, stamina, movement speed, dexterity }
+ * Current Assumptions Stamina regeneration is not considered and is fixed
+ *
+ * Each Battler can have 1:M hitboxes 
+ */
+
+public class Battler : MonoBehaviour
+{
+    //Battling Attributes 
+    public float maxHealth;
+    public float maxStamina;
+    public float baseAttack;
+    public float movementSpeed;
+    public float dexterity;
+    //consider maxMovement Speed (buffs may allow speed beyond max for fixed time)
+
+    //State attributes
+    public float stamina;
+    public float health;
+    public float lastAttackTime;
+    public BattlerState currentState;
+
+    //prefab component references
+    public Animator animator;
+    public Rigidbody2D rb;
+
+    /* When a MonoBehaviour is initialized,there is a sequence of function calls
+     * that take place
+     * 1. Constructor
+     * 2. Awake
+     * 3. Start
+     *
+     * In the game manager's start I initialize and set variables on the battlers,
+     * so its important that there default settings are set in awake and then
+     * overwritten in game manager's start
+     */
+    protected void Awake()
+    {
+        maxHealth = health = 10f;
+        maxStamina = stamina = 6f;
+        baseAttack = 4f;
+        movementSpeed = 2f;
+        dexterity = 1f; //lower is better (for now)
+
+        currentState = BattlerState.idle;
+
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+        lastAttackTime = -1;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //did the battler die?
+        if (health == 0)
+        {
+            Die();
+        }
+    }
+
+    //determine if the Battler will take damage
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //check if collider is of tag hitbox
+        if (collision.gameObject.tag == "Hitbox")
+        {
+            
+            //get the battler associated with the hit
+            Battler attacker = (Battler)collision.transform.parent.gameObject.GetComponent<Battler>();
+   
+            //is the attacker in an attack state?
+            if (attacker.GetBattlerState() == BattlerState.attack)
+            {
+                //deal damage (from parent class)
+                TakeDamage(attacker.GetDamage());
+                //start knockback (these are from the parent)
+                Knockback(attacker.transform);
+                StartCoroutine(KnockCo());
+
+
+            }
+        }
+    }
+
+
+    //perform knockback as a result of this Battler being attacked by another
+    public void Knockback(Transform tr)
+    {
+        if (rb != null)
+        {
+            rb.isKinematic = false;
+            currentState = BattlerState.hitStun;
+            Vector2 difference = transform.position - tr.position;
+            difference = difference.normalized * 5.0f;
+            rb.AddForce(difference, ForceMode2D.Impulse);
+        }
+    }
+
+    //Co-routine for handling the termination of knockback
+    public IEnumerator KnockCo()
+    {
+        if (rb != null)
+        {
+            yield return new WaitForSeconds(0.2f);
+            rb.velocity = Vector2.zero;
+            currentState = BattlerState.idle;
+            rb.isKinematic = true;
+        }
+    }
+
+    //All battlers can die!
+    public void Die()
+    {
+        //currentState = BattlerState.dead;
+        //rb.velocity = Vector2.zero;
+        //animator.SetTrigger("Dead");
+        Destroy(this.gameObject, 0f);
+    }
+
+    //All Battlers can take damage
+    public void TakeDamage(float dmg)
+    {
+        health -= dmg;
+        if (health < 0)
+        {
+            health = 0;
+        }
+    }
+
+    //How much damage (All things considered this battler deals
+    public float GetDamage()
+    {
+        return baseAttack;
+    }
+
+    //get the battler's state
+    public BattlerState GetBattlerState()
+    {
+        return currentState;
+    }
+
+    public void SetStats(float maxHealth, float maxStamina, float baseAttack, float movementSpeed, float dexterity)
+    {
+        this.maxHealth = maxHealth;
+        this.health = maxHealth;
+        this.maxStamina = maxStamina;
+        this.stamina = maxStamina;
+        this.baseAttack = baseAttack;
+        this.movementSpeed = movementSpeed;
+        this.dexterity = dexterity;
+    }
+
+
+    public List<float> GetStats()
+    {
+        List<float> statList = new List<float>();
+        statList.Add(maxHealth);
+        statList.Add(maxStamina);
+        statList.Add(baseAttack);
+        statList.Add(movementSpeed);
+        statList.Add(dexterity);
+
+        return statList;
+    }
+
+    public string PrintStats()
+    {
+        return "" + maxHealth + ":" + maxStamina + ":" + baseAttack + ":" + movementSpeed + ":" + dexterity;
+    }
+
+
+}
---
+++ SeniorProject-master/Assets/Battlers/Slime/Scripts/Slime.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//TO-Dos: Need to ensure all states are being accounted for
+// Need to fix code so it is more compact and portable
+// Need to move some elements to the Enemy Class if they can be used elsewhere
+public class Slime : Battler
+{
+    private float chaseRadius; //how far the slime can see
+    public Transform target; //the target of this Battler
+    private Collider2D Hitbox;
+    private Random random;
+
+
+  
+    void Awake()
+    {
+        base.Awake();
+        //initialize child specific variables
+        chaseRadius = 5f;
+        Hitbox = GetComponentInChildren<CircleCollider2D>();
+        Hitbox.enabled = false;
+    }
+
+    void FixedUpdate()
+    {
+        //fail safe to allow game manager to assign targets
+        if (target != null)
+        {
+            //Slime thinking
+            if (currentState != BattlerState.hitStun)
+            {
+                Think();
+            }
+
+            //toggle hitbox collider based on state
+            if (currentState == BattlerState.attack)
+            {
+                //enable hitbox
+                Hitbox.enabled = true;
+                StartCoroutine(AttackCo());
+            }
+            
+            
+        }
+    }
+
+    //determines if the target is in view, if so makes that slime moves towards it target
+    //having the slimes hitbox sit at the edge of the target's hurtbox
+    void Think()
+    {
+        /*
+
+        //get attackers hitbox center
+        Vector3 attackerHitboxCenter = transform.position + (Vector3) GetComponentInChildren<CircleCollider2D>().offset;
+        //attackers Hitbox radius
+        float ra = GetComponentInChildren<CircleCollider2D>().radius;
+        //get targets hurtbox center
+        Vector3 targetHurtboxCenter = target.position + (Vector3) GetComponent<CircleCollider2D>().offset;
+        //get targets hurtbox radius
+        float rt = GetComponent<CircleCollider2D>().radius;
+
+        //get the distance between the the center of the targets hurt box and the attackers hitbox
+        float attackerTargetDistance = Vector3.Distance(attackerHitboxCenter,targetHurtboxCenter) - ra - rt;
+        //the unit vector pointing to target
+        Vector3 TargetDirection = Vector3.Normalize(targetHurtboxCenter - attackerHitboxCenter);
+        //How far inside the targets hurtbox do we want to be?
+        float penetration = ra; //penetrate a 1/4 of the hitbox radius into the target
+
+        //the vector that points from the attacker's hitbox to the targets hurtbox
+        Vector3 TargetVector = TargetDirection * (attackerTargetDistance+penetration) + transform.position;
+
+
+        //Is the attackers hitbox inside the targets hurtbox?
+        //Is the attacker not in hitstun?
+        if (attackerTargetDistance < 0
+            && Time.time > (lastAttackTime + 1/maxStamina) && currentState != BattlerState.hitStun)
+        {
+            lastAttackTime = Time.time;
+            currentState = BattlerState.attack;
+        }
+
+        //if not move the slime towards the target
+        else if (attackerTargetDistance <= chaseRadius)
+        {
+            if ((currentState == BattlerState.idle || currentState == BattlerState.walk)
+                && (currentState != BattlerState.hitStun || currentState != BattlerState.dead))
+            {
+                //Vector3 temp = Vector3.MoveTowards(transform.position, TargetVector, movementSpeed * Time.deltaTime);
+                Vector3 temp = Vector3.MoveTowards(transform.position, TargetVector, movementSpeed * Time.deltaTime);
+                changeAnim(temp - transform.position);
+                rb.MovePosition(temp);
+                currentState = BattlerState.walk;
+          
+            }
+        }
+
+        */
+
+    }
+
+    //Co-routine for handling the termination of Attack State
+    public IEnumerator AttackCo()
+    {
+        if (rb != null)
+        {
+            yield return new WaitForSeconds(0.2f);
+            Hitbox.enabled = false;
+            currentState = BattlerState.idle;
+        }
+    }
+
+
+
+
+    void changeAnim(Vector2 direction)
+    {
+        direction = direction.normalized;
+
+        if (direction != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", direction.x);
+            animator.SetFloat("Vertical", direction.y);
+            animator.SetBool("Moving", true);
+        }
+    }
+
+  
+    //Accessors and Mutators
+    public void SetTarget(Transform tr)
+    {
+        //change the transform
+        target = tr;
+    }
+   
+
+    public List<float> GetStats()
+    {
+        List<float> statList = new List<float>();
+        statList.Add(maxHealth);
+        statList.Add(maxStamina);
+        statList.Add(baseAttack);
+        statList.Add(movementSpeed);
+        statList.Add(dexterity);
+        statList.Add(chaseRadius);
+
+        return statList;
+    }
+}
