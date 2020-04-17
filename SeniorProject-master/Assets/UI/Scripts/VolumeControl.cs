3e75f5a03d64b82a107f77d73bfeeecd6ab2cd02
Chrisfalotico <faloti24@students.rowan.edu>
2020-04-16 22:13:37 -0400

    Changed Fonts + UI look
    Changed the fonts and look of the UI, as well as touched up on the overall functionality. Volume sliders mostly work.

---
+++ SeniorProject-master/Assets/UI/Scripts/VolumeControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeControl : MonoBehaviour
+{
+
+    private AudioSource audioSrc;
+    private float soundVolume = 1f;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSrc.volume = soundVolume;
+    }
+
+    public void SetVolume(float vol)
+    {
+        soundVolume = vol;
+    }
+}
