5a7bc9645243ceea0001d7465dd238c9194a7617
Kyle Kaminski <kylekaminski26@gmail.com>
2020-04-20 12:34:03 -0400

    Revert "Revert "Revert "WorldGen and AStar integration"""

--- SeniorProject-master/Assets/WorldGen/Scripts/RoomTemplates.cs
+++ SeniorProject-master/Assets/WorldGen/Scripts/RoomTemplates.cs
@@ -13,58 +13,17 @@ public class RoomTemplates : MonoBehaviour
     public GameObject[] leftRoomsEnd;
     public GameObject[] rightRoomsEnd;
 
-    public GameObject[] overlays;
-
     public GameObject entryRoom;
     public GameObject closedRoom;
-    public GameObject endOverlay;
+    public GameObject endTrigger;
 
     public List<GameObject> rooms;
 
-    public int dungeonSize;
-    public float waitTime;
+    public int DungeonSize;
     public bool exitSpawned = false;
-
-    private bool scannedBoard = false;
-    public int maxX, minX, maxY, minY = 0;
 
     private void Start()
     {
         Instantiate(entryRoom, transform.position, Quaternion.identity);
-    }
-
-    private void Update()
-    {
-        if (!scannedBoard)
-        {
-            if (waitTime <= 0)
-            {
-                SetAndTriggerAStarScan();
-            }
-            else
-            {
-                waitTime -= Time.deltaTime;
-            }
-        }
-    }
-
-    private void SetAndTriggerAStarScan()
-    {
-        var graph = AstarPath.active.data.gridGraph;
-
-        //get the largest absolute value of max/min
-        int width = maxX > Mathf.Abs(minX) ? maxX : Mathf.Abs(minX);
-        int depth = maxY > Mathf.Abs(minY) ? maxY : Mathf.Abs(minY);
-
-        float nodeSize = 1;
-
-        //Account for the room spawnpoint to the walls (x + 10), double it for the actual width of the entire board
-        width = (width + 10) * 2;
-        depth = (depth + 10) * 2;
-
-        graph.SetDimensions(width, depth, nodeSize);
-
-        AstarPath.active.Scan();
-        scannedBoard = true;
     }
 }
