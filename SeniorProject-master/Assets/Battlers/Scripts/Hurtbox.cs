8ebbdf851f325423e15e13ce6532509472070909
Matthew <kevillem3@students.rowan.edu>
2020-04-22 21:03:47 -0400

    Battler/Slimes/Player
    Created hurtbox script
    Reformatted player and slime prefabs
    to use hurtbox and hurtbox script
    Adjusted playercontrol script to be more congruent with the battle parent
    Refactored the slime script to be more accurate in attacking
    Removed conflicting audio listener

---
+++ SeniorProject-master/Assets/Battlers/Scripts/Hurtbox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hurtbox : MonoBehaviour
+{
+    Battler parent;
+
+    //when this is instantiated get a reference to the parent
+    private void Awake()
+    {
+        parent = transform.parent.gameObject.GetComponent<Battler>();
+    }
+
+    //when the hurtbox is entered we alert the parent Battler object by calling
+    //a public function which takes in the collided object as a parameter
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        parent.OnHurtboxCollision(collision);
+    }
+}
