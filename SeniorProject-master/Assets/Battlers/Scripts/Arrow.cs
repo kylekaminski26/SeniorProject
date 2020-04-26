9c8f349f5d090b6ed2c567630c8ac714e1690158
Timmy G <gittti69@students.rowan.edu>
2020-04-26 15:01:34 -0400

    Ranger
    Arrows now deal damage and apply knockback

---
+++ SeniorProject-master/Assets/Battlers/Scripts/Arrow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : Battler
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        baseAttack = 10.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
