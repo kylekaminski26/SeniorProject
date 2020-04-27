fa5a51a6b827c34fc42a4f4704250d842b14f484
Timmy G <gittti69@students.rowan.edu>
2020-04-26 20:01:13 -0400

    Projectile changes
    Arrows will go through the player, but not walls. The ghost kings' bolts will go through walls, but not the player.

--- SeniorProject-master/Assets/Battlers/Ranger/Scripts/GhostBolt.cs
+++ SeniorProject-master/Assets/Battlers/Ranger/Scripts/GhostBolt.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Arrow : MonoBehaviour
+public class GhostBolt : MonoBehaviour
 {
     private int layerMask;
     private BoxCollider2D hitBox;
     // Start is called before the first frame update
     void Awake()
     {
-        layerMask =1 << 9;
+        layerMask = 1 << 10;
         hitBox = GetComponent<BoxCollider2D>();
         Invoke("enableCollider", 0.5f);
         Destroy(gameObject, 3.0f);
@@ -17,7 +17,7 @@ public class Arrow : MonoBehaviour
 
     private void Update()
     {
-        if(Physics2D.Raycast(transform.position, transform.position, 0.2f, layerMask))
+        if (Physics2D.Raycast(transform.position, transform.position, 0.2f, layerMask))
         {
             Destroy(gameObject);
         }
