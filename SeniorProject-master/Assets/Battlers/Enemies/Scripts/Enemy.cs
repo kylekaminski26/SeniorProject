a9fcd6545cf0cc2e6111a5c7e80aa718d5741e30
Matthew <kevillem3@students.rowan.edu>
2020-04-26 21:07:04 -0400

    Sorting Order (Enemies)
    Made enemies swap sorting order
    depending on the position of the player

--- SeniorProject-master/Assets/Battlers/Enemies/Scripts/Enemy.cs
+++ SeniorProject-master/Assets/Battlers/Enemies/Scripts/Enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemy : Battler
 {
+    //Awake and Update help define specific interactions between
+    //enemies and players, such as order in layer
+
     //State variables for AI thinking
     public enum AIState
     {
@@ -15,5 +18,38 @@ public class Enemy : Battler
         approach,
         attack,
         start
+    }
+
+    private bool playerExist = false;
+
+
+
+    public void Awake() {
+
+        base.Awake();
+        if (GameObject.Find("Player"))
+        {
+            playerExist = true;
+        }
+    }
+
+    public void Update()
+    {
+        base.Update();
+
+
+        //check if the player is above me, if the player is above me than swap
+        //my order in layer
+        if (playerExist) {
+            GameObject p = GameObject.Find("Player");
+            if (p.transform.position.y > transform.position.y)
+            {
+                sr.sortingOrder = 1;
+            }
+            else {
+                sr.sortingOrder = 0;
+            }
+        }
+
     }
 }
