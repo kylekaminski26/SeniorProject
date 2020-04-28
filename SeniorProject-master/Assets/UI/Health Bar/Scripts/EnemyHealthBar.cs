eb73887e5ee28a506216a000fee02f8fbc6ad7fb
caceresv <59008501+caceresv@users.noreply.github.com>
2020-04-27 23:26:23 -0400

    uhhh
    enemyhealthbar

--- SeniorProject-master/Assets/UI/Health Bar/Scripts/EnemyHealthBar.cs
+++ SeniorProject-master/Assets/UI/Health Bar/Scripts/EnemyHealthBar.cs
@@ -8,7 +8,7 @@ public class EnemyHealthBar : MonoBehaviour
     private Transform  healthBarFill; //Technically the pivot, but it works.
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         enemy = GetComponentInParent<Battler>();
         healthBarFill = transform.GetChild(0);
@@ -18,9 +18,12 @@ public class EnemyHealthBar : MonoBehaviour
     void Update()
     {
         float scaleFactor = enemy.health / enemy.maxHealth;
+        GetComponent<SpriteRenderer>().enabled = true;
+        transform.GetChild(0).GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
 
         //We should probably have it to where we get all the sprite renderers, put them into
         // an array, and disable/enable all of the them. But do we really need an array of 2 objects.
+        /*
         if (scaleFactor == 1)
         {
             GetComponent<SpriteRenderer>().enabled = false;
@@ -31,7 +34,7 @@ public class EnemyHealthBar : MonoBehaviour
             GetComponent<SpriteRenderer>().enabled = true;
             transform.GetChild(0).GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
         }
-
+        */
         healthBarFill.transform.localScale = new Vector3(scaleFactor, 1, 1);
     }
 }
